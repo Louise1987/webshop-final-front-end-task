20ae765a0427bd420afe341abe93101d819244f2
Louise1987 <31926492+Louise1987@users.noreply.github.com>
2017-12-28 15:47:36 +0100

    ny

--- script.js
+++ script.js
@@ -1,3 +1,77 @@
 $(document).ready(function(){
 
+
+var users;
+var products;
+
+fetch("./kunder.json")
+.then(function(response) {
+    return response.json();
+})
+then(function(ourUsers) {
+    users = ourUsers;
+});
+
+if(sessionStorage.saveUser != null ){
+    thisUserIsLoggedIn();
+}else{
+
+    $(".buttonForm").click(function(){
+        for(var i = 0; i < users.length; i++){
+    
+        //Om password och username stämmer loggas användare in och sparas i sessionstorage
+        if( $(".mailForm").val() == users[i].username && $(".passwordForm").val() == users[i].password){
+
+            thisUserIsLoggedIn();
+            sessionStorage.saveUser = users[i].username;
+
+            }else{
+                
+                $(".forgotPassword").show();
+            }
+        }
+    })
+}
+
+$(".logOutButton").click(function(){
+    sessionStorage.removeItem("saveUser");
+    location.reload();
+
+
+});
+
+$(".div2").hide();
+$(".div3").hide();
+$(".div4").hide();
+
+
+$(".knapp1").click(function(){
+    $(".div1").slideDown("1000");
+    $(".div2").hide();
+    $(".div3").hide();
+    $(".div4").hide();
+});
+
+$(".knapp2").click(function(){
+    $(".div2").slideDown("1000");
+    $(".div1").hide();
+    $(".div3").hide();
+    $(".div4").hide();
+});
+
+$(".knapp3").click(function(){
+    $(".div3").slideDown("1000");
+    $(".div1").hide();
+    $(".div2").hide();
+    $(".div4").hide();
+});
+
+$(".knapp4").click(function(){
+    $(".div4").slideDown("1000");
+    $(".div1").hide();
+    $(".div2").hide();
+    $(".div3").hide();
+});
+
+
 });
